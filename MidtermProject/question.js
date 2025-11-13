db9eb4f7fea155997274c519468aa6aa330d3244
AbigailNitro <decepticonn05@gmail.com>
2025-11-13 15:23:38 -0800

    started work of questionaire

---
+++ MidtermProject/question.js
@@ -0,0 +1,15 @@
+let form=
+document.getElementById('favChar'); form.addEventListener('submit',function(event) {
+
+    event.preventDefault();
+
+    let Character =
+    document.getElementById('character').value;
+
+    switch
+
+
+}
+
+
+);
